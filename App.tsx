2f574ed753157a132e5a21c026bcf55a6d5d16ed
fashander <tobyton2008@yahoo.co.uk>
2020-12-08 23:44:54 +0100

    Change run environment to AVD

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container} >
-      <Text style={styles.title}>LAWnI</Text>
+      <Text style={styles.title}>LAWnI Sch</Text>
     </View>
   );
 }
