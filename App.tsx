f06a3669f8f1a242f774933f109309944cdfdef6
fashander <tobyton2008@yahoo.co.uk>
2020-12-15 23:49:59 +0100

    New Screen

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Calendar, CalendarList, Agenda, DateObject} from "react-native-calendars";
