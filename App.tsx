10c235883efb8c2dc0c80c38219d4f4f13d0f540
fashander <tobyton2008@yahoo.co.uk>
2020-12-21 00:55:24 +0100

    Created 2 tabs and assigned pages

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import {Calendar, CalendarList, Agenda, DateObject} from "react-native-calendars";
 import Home from "./src/components/screen/homepage";
 import Main from "./src/components/screen/main";
-
+import Appnin from "./src/app"
 
 // const [open, setOpen]=useState(false);
 // const [date, setDate]=useState<DateObject>()
@@ -14,7 +14,7 @@ import Main from "./src/components/screen/main";
 // }
 export default function App() {
   return (
-      <Main/>
+      <Appnin/>
   );
 }
 
--- src/app.tsx
+++ src/app.tsx
@@ -1,86 +1,25 @@
-// import React from 'react';  
-// import {StyleSheet, Text, View,Button} from 'react-native';  
-// // import { createBottomTabNavigator, createAppContainer} from 'react-navigation';  
-// // import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';  
-// class HomeScreen extends React.Component {  
-//   render() {  
-//     return (  
-//         <View style={styles.container}>  
-//           <Text>Home Screen</Text>  
-//         </View>  
-//     );  
-//   }  
-// }  
-// class ProfileScreen extends React.Component {  
-//   render() {  
-//     return (  
-//         <View style={styles.container}>  
-//           <Text>Profile Screen</Text>  
-//         </View>  
-//     );  
-//   }  
-// }  
-// class ImageScreen extends React.Component {  
-//     render() {  
-//         return (  
-//             <View style={styles.container}>  
-//                 <Text>Image Screen</Text>  
-//             </View>  
-//         );  
-//     }  
-// }  
-// class CartScreen extends React.Component {  
-//     render() {  
-//         return (  
-//             <View style={styles.container}>  
-//                 <Text>Cart Screen</Text>  
-//             </View>  
-//         );  
-//     }  
-// }  
-// const styles = StyleSheet.create({  
-//     container: {  
-//         flex: 1,  
-//         justifyContent: 'center',  
-//         alignItems: 'center'  
-//     },  
-// });  
-// const TabNavigator = createMaterialBottomTabNavigator(  
-//     {  
-//         Home: { screen: HomeScreen,  
-//             navigationOptions:{  
-//                 tabBarLabel:'Home',  
-//             }  
-//         },  
-//         Profile: { screen: ProfileScreen,  
-//             navigationOptions:{  
-//                 tabBarLabel:'Profile',   
-//                 activeColor: '#f60c0d',  
-//                 inactiveColor: '#f65a22',  
-//                 barStyle: { backgroundColor: '#f69b31' },  
-//             }  
-//         },  
-//         Image: { screen: ImageScreen,  
-//             navigationOptions:{  
-//                 tabBarLabel:'History',    
-//                 activeColor: '#615af6',  
-//                 inactiveColor: '#46f6d7',  
-//                 barStyle: { backgroundColor: '#67baf6' },  
-//             }  
-//         },  
-//         Cart: {  
-//             screen: CartScreen,  
-//             navigationOptions:{  
-//                 tabBarLabel:'Cart',    
-//             }  
-//         },  
-//     },  
-//     {  
-//       initialRouteName: "Home",  
-//       activeColor: '#f0edf6',  
-//       inactiveColor: '#226557',  
-//       barStyle: { backgroundColor: '#3BAD87' },  
-//     },  
-// );  
-  
-// export default createAppContainer(TabNavigator);  
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from "./components/screen/homepage";
+import Main from "./components/screen/main"
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Calendar" component={Main} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
